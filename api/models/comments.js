09a9d7e8d585e262a885f74718179c26db000815
Jonatan <jonatansg@hotmail.com>
2021-03-09 20:07:30 +0000

    Created comments branch and comments model

---
+++ api/models/comments.js
@@ -0,0 +1,29 @@
+const Mongoose = require('mongoose')
+const commentsSchema = new Mongoose.Schema({
+  user: {
+    type: Mongoose.Types.ObjectId,
+    required: true,
+  },
+  event: {
+    type: Mongoose.Types.ObjectId,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  responses: {
+    type: [commentsSchema],
+  },
+  likes: {
+    type: [Mongoose.Types.ObjectId],
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+})
+
+const commentsModel = Mongoose.model('comments', eventSchema)
+
+module.exports = commentsModel
