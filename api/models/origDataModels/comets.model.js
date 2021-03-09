37685a09da1f3e4080dd4d99e4f63ee497cfc3f9
Jonatan <jonatansg@hotmail.com>
2021-03-09 23:19:59 +0000

    Created models data comets and meteor showers

---
+++ api/models/origDataModels/comets.model.js
@@ -0,0 +1,53 @@
+const Mongoose = require('mongoose')
+const cometsSchema = new Mongoose.Schema({
+  object: {
+    type: String,
+    required: true,
+  },
+  peDate: {
+    type: Date,
+    required: true,
+  },
+  peRa: {
+    type: String,
+    required: true,
+  },
+  peDec: {
+    type: String,
+    required: true,
+  },
+  peCons: {
+    type: String,
+    required: true,
+  },
+  paMa: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  constellation: {
+    type: String,
+    required: true,
+  },
+  visibleFrom: {
+    type: Date,
+    required: true,
+  },
+  visibleUntil: {
+    type: Date,
+    required: true,
+  },
+  highest: {
+    type: Date,
+    required: true,
+  },
+  ra: {
+    type: String,
+    required: true,
+  },
+})
+
+const cometsModel = Mongoose.model('comets', cometsSchema)
