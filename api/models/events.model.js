dbcd8ac315a95b2eeba6e70585b7b7a990ec5210
Norber <nox_nox_@hotmail.com>
2021-03-09 20:27:45 +0000

    Events model done

---
+++ api/models/events.model.js
@@ -0,0 +1,64 @@
+const Mongoose = require('mongoose')
+const eventSchema = new Mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: [
+      'eclipseMoon',
+      'eclipseSun',
+      'planets',
+      'meteorShower',
+      'comets',
+      'conjunction',
+    ],
+    required: true,
+  },
+  moon: {
+    type: String,
+    //********************
+    // OJO -> TODO find enum values in api
+    /*enum: [
+      'Nueva',
+      'Cuarto creciente',
+      'Gibosa creciente',
+      'Llena',
+      'Gibosa menguante',
+      'Cuarto menguante',
+    ],*/
+    required: true,
+  },
+  weather: {
+    type: String,
+    //********************
+    // OJO -> TODO find enum values in api
+    //enum: []
+    required: true,
+  },
+  comments: {
+    type: [Mongoose.Types.ObjectId],
+    required: false,
+  },
+  origData: {
+    type: Mongoose.Types.ObjectId,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+})
+
+const eventModel = Mongoose.model('event', eventSchema)
+
+module.exports = eventModel
