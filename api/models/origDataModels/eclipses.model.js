61d4a49e9caab0595819a884f8807bfd74b17598
Norber <nox_nox_@hotmail.com>
2021-03-09 23:18:52 +0000

    Origdata models and controllers

--- api/models/origDataModels/eclipses.model.js
+++ api/models/origDataModels/eclipses.model.js
@@ -2,6 +2,28 @@ const Mongoose = require('mongoose')
 const eclipsesSchema = new Mongoose.Schema({
   sl: {
     type: String,
+    enum: ['Lunar', 'Solar'],
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['Total', 'Annular', 'Partial', 'Hybrid', 'Appulse'],
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  coorsZodiac: {
+    type: String,
+    required: true,
+  },
+  magnitude: {
+    type: Number,
     required: true,
   },
 })
