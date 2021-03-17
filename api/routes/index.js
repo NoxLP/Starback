88c0bffc837f70f858537e35df24c7976a31c69e
Norber <nox_nox_@hotmail.com>
2021-03-17 08:07:17 +0000

    Changed status to api route

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,8 +5,13 @@ const eventsRouter = require('./events.router')
 const commentsRouter = require('./comments.router')
 const { authUser } = require('../utils/index') // Authenticated Route
 
+router.use('/status', status)
 router.use('/auth', authRouter)
 router.use('/events', authUser, eventsRouter)
 router.use('/comments', authUser, commentsRouter)
 
 module.exports = router
+
+function status(req, res) {
+  res.status(200).json("I'm alive, thanks for asking.\n\nBye!")
+}
